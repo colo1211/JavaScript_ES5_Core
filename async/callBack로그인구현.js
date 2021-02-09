2a17cb2ddb845fa2c6c69fdbf5484a5aeabf26fb
colo1211 <colo1211@naver.com>
2021-02-09 13:23:17 +0900

    callBack

--- async/callBack로그인구현.js
+++ async/callBack로그인구현.js
@@ -1,32 +1,35 @@
-class UserStorage{
-    loginUser(id, password, onSuccess, onError){
+class UserStorage {
+    loginUser(id, password, onSuccess, onError) {
         setTimeout(()=>{
-            if (id === 'ellie' && password==='dream' || id === 'coder' && password === 'academy'){
+            if (id === 'ellie' && password === 'dream' || id === 'coder' && password === 'academy'){
                 onSuccess(id);
             }else {
-                onError(new Error('조회되는 ID가 없습니다.'));
+                onError(new Error('조회된 ID가 없습니다.'));
             }
         },2000);
     }
+
     getRoles(user, onSuccess, onError){
         setTimeout(()=>{
             if (user === 'ellie'){
-                onSuccess({name: 'ellie', role: 'admin'});
+                onSuccess({name : 'ellie', role: 'admin'});
             }else {
-                onError(new Error('접근권한이 존재하지 않습니다.'));
+                onError(new Error(`${user}님 께서는 접근 권한이 존재하지 않습니다.`));
             }
         },1000);
     }
 }
 
-const id = prompt('id를 입력하세요!');
-const password = prompt('password를 입력하세요!');
-const newUser= new UserStorage();
+const id = prompt('ID를 입력하세요');
+const password = prompt('PassWord를 입력하세요');
 
-newUser.loginUser(id, password,
+const newUser = new UserStorage();
+newUser.loginUser(id , password,
     (user)=>{
-    newUser.getRoles(user,(userInfo)=> {
-        console.log(userInfo);
-        alert(`${userInfo.name}님!, ${userInfo.role} 권한이 있습니다!`);
-    }, (error)=>{alert(error)});
-}, (error)=>{alert(error)});
+        newUser.getRoles(user, (userId)=>{
+            alert(`${userId.name} 님! ${userId.role}이 있습니다!`);
+        }, (error)=>{ // 접근 권한이 없습니다.
+            alert(error);
+        });
+    },
+    (error)=>{alert(error)}); // 조회 가능한 ID가 없습니다.
