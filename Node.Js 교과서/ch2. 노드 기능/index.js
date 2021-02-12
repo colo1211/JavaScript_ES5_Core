98b85643cc5b3b2678cd80ef1bb4399feb2e455c
colo1211 <colo1211@naver.com>
2021-02-13 03:19:47 +0900

    ES6 문법 정리

--- Node.Js 교과서/ch2. 노드 기능/index.js
+++
@@ -1,8 +0,0 @@
-const {odd, even}= require('./var');
-const checkOddOrEven= require('./func').checkOddOrEven; // 객체로 온 것 중에서 함수만
-
-console.log(checkOddOrEven(10));
-
-// 모듈화 : 파일들 간의 중복을 제거
-// module.exports = {}, 파일 당 한번만 가능
-// require로 받아온다.
